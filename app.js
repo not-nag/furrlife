754208ad8bab8490a3c99fbc199b5fa7949affce
Nagraj Thamankar <nagrajtham@gmail.com>
2022-12-12 00:00:55 +0530

    Updated input form

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const exp = require("constants");
 const express = require("express");
 const app = express();
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const path = require("path");
+const bodyParser = require("body-parser");
+const { strictEqual } = require("assert");
+const { join } = require("path");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "furrlife",
+});
 
 app.get("/", function (req, res) {
   res.render("index");
@@ -19,6 +31,38 @@ app.get("/index.ejs", function (req, res) {
 
 app.get("/list.ejs", function (req, res) {
   res.render("list");
+});
+
+app.post("/list.ejs", function (req, res) {
+  var name = req.body.name;
+  var city = req.body.city;
+  var contact_no = req.body.contact_no;
+  var type = req.body.type;
+  var breed = req.body.breed;
+  var vaccinated = req.body.vaccinated;
+  var photo = req.body.photo;
+
+  connection.connect(function (err) {
+    if (err) {
+      return console.error("error: " + err.message);
+    }
+    var sql =
+      "INSERT INTO pet_listings (NAME, CITY, CONTACT_NO, TYPE, BREED, VACCINATED, PHOTO) VALUES (?,?,?,?,?,?,?)";
+    connection.query(
+      sql,
+      [name, city, contact_no, type, breed, vaccinated, photo],
+      function (err, result) {
+        if (err) {
+          return console.error("error: " + err.message);
+        }
+      }
+    );
+  });
+  res.render("success");
+});
+
+app.get("/success.ejs", function (req, res) {
+  res.render("success");
 });
 
 app.listen(3000, function () {
